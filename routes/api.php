d98365d44fa091cb74e095e1d64d090c40b9203c
Adis <skawan13@gmail.com>
2022-07-28 07:02:55 +0700

    update routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +17,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// user endpoints
+Route::post('login', [ApiController::class, 'authenticate']);
+Route::post('register', [ApiController::class, 'register']);
 
-Route::get('/transaction');
+Route::group(['middleware' => ['jwt.verify']], function() {
+    
+    // user endpoints
+    Route::get('logout', [ApiController::class, 'logout']);
+    Route::get('get_user', [ApiController::class, 'get_user']);
+    
+    //customer endpoints
+    Route::get('customer', [CustomerController::class, 'index']);
+    Route::get('customerWithPaginate', [CustomerController::class, 'pagination']);
+    Route::get('customerGetDetail/{id}', [CustomerController::class, 'show']);
+    Route::post('customer/insert', [CustomerController::class, 'store']);
+    Route::put('customer/update/{customer}',  [CustomerController::class, 'update']);
+    Route::delete('customer/delete/{customer}',  [CustomerController::class, 'destroy']);
+    Route::post('customerSearchbyName', [CustomerController::class, 'searchbyname']);
+
+    //order endpoints
+    Route::get('order', [OrderController::class, 'index']);
+    Route::get('orderWithPaginate', [OrderController::class, 'pagination']);
+    Route::get('orderGetDetail/{id}', [OrderController::class, 'show']);
+    Route::post('order/insert', [OrderController::class, 'store']);
+    Route::put('order/update/{order}',  [OrderController::class, 'update']);
+    Route::delete('order/delete/{order}',  [OrderController::class, 'destroy']);
+    Route::post('orderSearchbyName', [OrderController::class, 'searchbyname']);
+
+});
